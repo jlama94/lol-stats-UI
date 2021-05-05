c32b060a4a11b74d37bbeee354bf2aa0802d4d5c
Julio <lamajulio17@gmail.com>
2021-05-05 09:09:29 -0500

    changes to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import {HomeComponent} from "./home/home.component";
 import {StackedBarChartComponent} from './stacked-bar-chart/stacked-bar-chart.component';
 
 const routes: Routes = [
-  {path: 'stats', component: HomeComponent},
+  {path: 'bar-chart', component: HomeComponent},
   {path: 'stacked-bar-chart', component: StackedBarChartComponent},
-  {path: '', redirectTo: '/stats', pathMatch: 'full'}
+  {path: '', redirectTo: '/bar-chart', pathMatch: 'full'}
 ];
 
 @NgModule({
