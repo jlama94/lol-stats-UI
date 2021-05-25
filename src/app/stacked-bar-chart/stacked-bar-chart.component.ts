97c3e6fc8954796fe7868a19ee1338ecd2976091
Julio <lamajulio17@gmail.com>
2021-05-25 12:43:36 -0500

    Adding new chart with dynamic variables

--- src/app/stacked-bar-chart/stacked-bar-chart.component.ts
+++ src/app/stacked-bar-chart/stacked-bar-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import {MatchesService} from '../httpClient/services/matches.service';
 
 @Component({
   selector: 'app-stacked-bar-chart',
@@ -10,61 +11,76 @@ export class StackedBarChartComponent implements OnInit {
 
   championNames = ['Yone', 'Soraka', 'Teemo'];
 
-  constructor() { }
+  seriesType: 'column';
+
+  chartReady = false;
+
+  subtitle: string;
+
+  constructor(private service: MatchesService) { }
 
   ngOnInit(): void {
+    // this.service.getMatchHistory().subscribe((value => {
+    //   console.log(value);
+    // }))
   }
 
-  renderChart() {
-    Highcharts.chart({
-      chart: {
-        renderTo: 'container',
-        type: 'column',
-        style: {
-          fontFamily: 'Times New Roman'
+  renderChart()
+  {
+
+    this.service.getMatchHistory().subscribe(value => {
+      Highcharts.chart({
+        chart: {
+          renderTo: 'container',
+          type: 'column',
+          style: {
+            fontFamily: 'Times New Roman'
+          },
+          shadow: true
         },
-        shadow: true
-      },
-      title: {
-        text: 'Wins/Losses',
-      },
-      subtitle: {
-        text: ''
-      },
-      tooltip: {
-        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
-        shared: true
-      },
-      xAxis: {
-        categories : this.championNames
-      },
-      yAxis: {
         title: {
-          text: 'Wins/Losses per Champion'
-        }
-      },
-      plotOptions: {
-        column: {
-          stacking: 'percent',
-          dataLabels: {
-            enabled: true
-          }
-        }
-      },
-      series: [
-        {
-          name: 'Wins',
-          type: 'column',
-          data: [3,  1, 7]
+          text: 'Wins/Losses',
         },
-        {
-          name: 'Losses',
-          type: 'column',
-          data: [1, 4, 2]
-        }
-      ]
+        subtitle: {
+          text: ''
+        },
+        tooltip: {
+          pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+          shared: true
+        },
+        xAxis: {
+          categories : this.championNames
+        },
+        yAxis: {
+          title: {
+            text: 'Wins/Losses per Champion'
+          }
+        },
+        plotOptions: {
+          series: {
+
+          },
+          column: {
+            stacking: 'percent',
+            dataLabels: {
+              enabled: true
+            },
+          }
+        },
+        series: [
+          {
+            name: value.win.name,
+            type: this.seriesType,
+            data: value.win.wins
+          },
+          {
+            name: value.loss.name,
+            type: this.seriesType,
+            data: value.loss.losses
+          }
+        ]
+      });
     });
+
   }
-
-
 }
